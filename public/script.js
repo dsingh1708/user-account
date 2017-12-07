089b2fd2490389c01ad3af5def7b41740040a5e7
dsingh1708 <30971643+dsingh1708@users.noreply.github.com>
2017-12-07 13:03:03 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,41 +1,4 @@
 var app = angular.module('userApp',['ngRoute']);
-
-// app.controller('myCtrl', function($scope,$http) {
-
-//     $scope.post = function() {
-//         var payload = {
-//           name:$scope.unameR,
-//           email:$scope.emailR,
-//           pwd:$scope.pwdR
-//         };
-
-// $http({
-//   method: 'POST',
-//  url: 'http://10.132.33.164:8086/Postbird',
-//   // url: 'http://localhost:8086/Postbird',
-//   data:payload,
-//   headers:{'Content-Type': 'application/json'},
-// }).then(function successCallback(response) {
-//       console.log("Entered in successCallback ");
-//       console.log(JSON.stringify(response.data));
-//       console.log(response.status);
-//       console.log(response.statusText);
-//       console.log(response.statusText);
-//       alert('Success');
-
-//   }, function errorCallback(response) {
-//       console.log("Entered in errorCallback ");
-//       console.log(response.xhrStatus);
-//       console.log(response.status);
-//       console.log(response.statusText);
-// });   
-//       };
-
-
-// });
-
-
-
 app.config(function($routeProvider) {
       $routeProvider
  
